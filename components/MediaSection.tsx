f17bdd2099877fa59d2c6ad5c77b8788d40878e7
kushin <dolimberio.a12242640@umak.edu.ph>
2025-10-27 18:10:55 +0800

    Add GIF video assets and update MediaSection
    Added combat.gif, overworld.gif, and title.gif to assets/videos. Updated MediaSection.tsx to use GIFs instead of PNGs for certain media items. Extended type declarations to support importing .gif files.

--- components/MediaSection.tsx
+++ components/MediaSection.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import Section from './ui/Section';
 import SectionTitle from './ui/SectionTitle';
-import bathalaTitle from '../assets/images/bathala_title.png';
+import bathalaTitle from '../assets/videos/title.gif';
 import combat from '../assets/images/combat.png';
 import overworld_map from '../assets/images/overworld_map.png';
-import bathala_shop from '../assets/images/bathala_shop.png';
-import combat_2 from '../assets/images/combat_2.png';
+
+import overworldGif from '../assets/videos/overworld.gif';
+import combatgif from  "../assets/videos/combat.gif";
 
 const MediaImage = ({ src, alt }: { src: string; alt: string }) => (
   <div className="aspect-video bg-[#000] border-2 border-[#77888C]/30">
@@ -21,11 +22,11 @@ const MediaSection: React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <MediaImage src={combat} alt="Combat scene showing a Full House" />
         <MediaImage src={overworld_map} alt="The overworld map at night" />
-        <MediaImage src={bathala_shop} alt="The Mysterious Merchant's shop interface" />
-        <MediaImage src={combat_2} alt="The final boss, the False Bathala" />
-    <div className="sm:col-span-2">
-      <MediaImage src={bathalaTitle} alt="The prologue storyboard" />
-    </div>
+        <MediaImage src={overworldGif} alt="The Mysterious Merchant's shop interface" />
+        <MediaImage src={combatgif} alt="The final boss, the False Bathala" />
+        <div className="sm:col-span-2">
+          <MediaImage src={bathalaTitle} alt="The prologue storyboard" />
+        </div>
       </div>
     </Section>
   );
